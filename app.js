45b3dc42ce25d26cb8fc872e18b5ce3086ccddc0
ArmineSargsyan1 <armine9086@gmail.com>
2025-11-14 17:08:21 +0400

    change app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-console.log("kuku");
+console.log("kuku-lala123");
 
 
 console.log("hello git1");
 console.log("hello git2");
 console.log("hello git3");
+console.log("hello git4");
+console.log("hello git5");
